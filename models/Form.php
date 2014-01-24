d18ac0154fe0521d0e4d72a03b5e8e9eb5bfee2c
Denis <bulkak-seo@yandex.ru>
2014-01-24 13:01:54 +0300

    Update Form.php

--- models/Form.php
+++ models/Form.php
@@ -9,7 +9,8 @@ class Form extends CFormModel
 {
     public $file;
     public $url;
-
+    public $tempfile;
+    
     public function rules()
     {
         return array(
@@ -18,19 +19,73 @@ class Form extends CFormModel
         );
     }
 
-    public function saveModule()
+    public function saveModule($moduleName)
     {
+         $tempPath = Yii::app()->controller->module->getTempPath();
+         $tempCat = $tempPath.'/'.$moduleName;
+         $todir = Yii::app()->controller->module->getModulesPath();
+         $todir = $todir.'/'.$moduleName;
+         CFileHelper::copyDirectory($tempCat, $todir);
+         CFileHelper::removeDirectory($tempCat);
+         //rmdir($tempCat);
+         unlink($this->tempfile);
+    }
+    
+    public function validate() {
+        $zip = new ZipArchive();
         $file = CUploadedFile::getInstance($this,'file');
         $tempPath = Yii::app()->controller->module->getTempPath();
-        $moduleName = str_replace('.phar.zip','',$file->name);
-        $modulePath = Yii::app()->controller->module->getModulesPath().'/'.$moduleName;
-        $tempFile = $tempPath.'/'.$file->name;
-        if($file->saveAs($tempFile)){
-            $phar = new Phar($tempFile);
-            $phar->extractTo($modulePath, null, true);
-            copy($modulePath.'/install/'.$moduleName.'.php', Yii::getPathOfAlias('application.config.modules').'/'.$moduleName.'.php');
-            unlink($tempFile);
+        $this->tempfile = $tempFile = $tempPath.'/'.$file->name;
+        // сохраним во временную папку
+        $file->saveAs($tempFile);
+        $moduleName = "";
+        if ($zip->open($tempFile) == true) {
+            // пройдемся по архиву, проверим, есть ли там класс модуля, заодно если есть извлечем из него имя модуля
+            for($i = 0; $i < $zip->numFiles; $i++)
+            {
+                $filename = $zip->getNameIndex($i);
+                if (strstr($filename, "Module.php") && !strstr($filename, "/") ){
+                    $moduleName = mb_strtolower(str_replace("Module.php", "", $filename));
+                }
+            }
         }
+        else {
+            unlink($this->tempfile);
+            $this->addError('attr', "Что-то не так с архивом, проверьте.. (.zip?)");
+            return false;
+        }
+        if ($moduleName) {
+            // хорошо бы уточнить, нет ли уже у нас такого модуля?
+            foreach (scandir (Yii::app()->controller->module->getModulesPath()) as $name) {
+                if ($name == $moduleName) {
+                    // уже есть, что делать будем?
+                    $zip->close();
+                    unlink($this->tempfile);
+                    $this->addError('attr', "У же есть такой модуль.. Можно было-бы его заменить по согласию, но это позже");
+                    return false;
+                }
+            }
+            // если нашли файл модуля, то скорее всего все хорошо, распакуем архив
+            $zip->extractTo($tempPath.'/'.$moduleName);
+            $zip->close();
+            return $moduleName;
+        }
+        else {
+            unlink($this->tempfile);
+            $this->addError('attr', "Не корректная струтура каталогов модуля, мы не нашли основной файл модуля");
+            return false;
+        }
+    }
+    
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'file'             => Yii::t('ModInstallerModule.modinstaller', 'file'),
+            'url'      => Yii::t('ModInstallerModule.modinstaller', 'url'),
+        );
     }
 
 
